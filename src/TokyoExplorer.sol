ce96c6b4cfa6fc544b711f65d5e0b59ef8bf6cb6
James Dunn <james.ed.dunn@gmail.com>
2023-03-22 03:40:01 +0900

    update badges

--- src/TokyoExplorer.sol
+++ src/TokyoExplorer.sol
@@ -26,7 +26,7 @@ contract TokyoExplorer is ERC721A, Ownable {
     mapping(address => uint256) public tokenOf;
 
     // collection of image offset coordinates and text for reward stamps
-    string[3][23] public stamps;
+    string[5][23] public stamps;
 
     uint256 public immutable cost = 0.08 ether;
 
@@ -36,32 +36,30 @@ contract TokyoExplorer is ERC721A, Ownable {
     // address where base svg image is stored
     address private baseSvgPointer;
 
-    // TODO: replace these placeholder coordinates!
-    // might need custom scale value as well based on character length
     constructor() ERC721A("TOKYO 23", "TOKYO23") {
-        stamps[0] = ["7.7", "11", unicode"千代田"];
-        stamps[1] = ["7.7", "11", unicode"中央"];
-        stamps[2] = ["7.7", "11", unicode"港"];
-        stamps[3] = ["7.7", "11", unicode"新宿"];
-        stamps[4] = ["7.7", "11", unicode"文京"];
-        stamps[5] = ["7.7", "11", unicode"台東"];
-        stamps[6] = ["7.7", "11", unicode"墨田"];
-        stamps[7] = ["7.7", "11", unicode"江東"];
-        stamps[8] = ["7.7", "11", unicode"品川"];
-        stamps[9] = ["175.2", "317.8", unicode"目黒"];
-        stamps[10] = ["7.7", "11", unicode"大田"];
-        stamps[11] = ["7.7", "11", unicode"世田谷"];
-        stamps[12] = ["7.7", "11", unicode"渋谷"];
-        stamps[13] = ["7.7", "11", unicode"中野"];
-        stamps[14] = ["7.7", "11", unicode"杉並"];
-        stamps[15] = ["7.7", "11", unicode"豊島"];
-        stamps[16] = ["7.7", "11", unicode"北"];
-        stamps[17] = ["7.7", "11", unicode"荒川"];
-        stamps[18] = ["7.7", "11", unicode"板橋"];
-        stamps[19] = ["7.7", "11", unicode"練馬"];
-        stamps[20] = ["7.7", "11", unicode"足立"];
-        stamps[21] = ["7.7", "11", unicode"葛飾"];
-        stamps[22] = ["7.7", "11", unicode"江戸川"];
+        stamps[0] = ["297.5", "225.5", "8672cb", "645597", unicode"千代田"];
+        stamps[1] = ["348.5", "237.5", "f7da00", "c4ad00", unicode"中央"];
+        stamps[2] = ["266.5", "310.5", "6cb0d2", "51849e", unicode"港"];
+        stamps[3] = ["230.5", "199.5", "c32d76", "8a2057", unicode"新宿"];
+        stamps[4] = ["278.5", "143.5", "52bb81", "3a875e", unicode"文京"];
+        stamps[5] = ["370.5", "156.5", "cd394b", "9c2a38", unicode"台東"];
+        stamps[6] = ["418.5", "207.5", "3860b5", "274482", unicode"墨田"];
+        stamps[7] = ["400.5", "310.5", "489dc3", "34728f", unicode"江東"];
+        stamps[8] = ["194.5", "359.5", "6651bc", "493a87", unicode"品川"];
+        stamps[9] = ["144.5", "334.5", "d65477", "a5405c", unicode"目黒"];
+        stamps[10] = ["132.5", "451.5", "d885f8", "aa69c4", unicode"大田"];
+        stamps[11] = ["37.5", "338.5", "74d375", "57a058", unicode"世田谷"];
+        stamps[12] = ["216.5", "280.5", "b4cc2a", "879920", unicode"渋谷"];
+        stamps[13] = ["141.5", "203.5", "e34262", "b2334d", unicode"中野"];
+        stamps[14] = ["89.5", "236.5", "578d38", "375a23", unicode"杉並"];
+        stamps[15] = ["196.5", "129.5", "bba46a", "87994d", unicode"豊島"];
+        stamps[16] = ["249.5", "45.5", "c4d1cc", "949e9a", unicode"北"];
+        stamps[17] = ["330.5", "90.5", "45853a", "2a5524", unicode"荒川"];
+        stamps[18] = ["170.5", "35.5", "539a8e", "37665e", unicode"板橋"];
+        stamps[19] = ["47.5", "100.5", "dd7508", "ac5a08", unicode"練馬"];
+        stamps[20] = ["379.5", "38.5", "c557d1", "93419e", unicode"足立"];
+        stamps[21] = ["468.5", "79.5", "6d9365", "475f42", unicode"葛飾"];
+        stamps[22] = ["521.5", "229.5", "90e8cf", "70b4a1", unicode"江戸川"];
 
         _initializeOwner(msg.sender);
     }
@@ -109,15 +107,15 @@ contract TokyoExplorer is ERC721A, Ownable {
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
 
-        string[3][] memory unlockedStamps = retrieveUnlocks(tokenId);
+        string[5][] memory unlockedStamps = retrieveUnlocks(tokenId);
         bytes memory encodedAttributes;
         for (uint256 i = 0; i < unlockedStamps.length; i++) {
-            string[3] memory stamp = unlockedStamps[i];
+            string[5] memory stamp = unlockedStamps[i];
             if (i == 0) {
                 encodedAttributes = ",";
             }
 
-            encodedAttributes = abi.encodePacked(encodedAttributes, '{"value":"', stamp[2], '"}');
+            encodedAttributes = abi.encodePacked(encodedAttributes, '{"value":"', stamp[4], '"}');
             if (i < unlockedStamps.length - 1) {
                 encodedAttributes = abi.encodePacked(encodedAttributes, ",");
             }
@@ -166,20 +164,27 @@ contract TokyoExplorer is ERC721A, Ownable {
         emit UnlocksApplied(tokenId, unlockMap);
     }
 
-    function buildSvg(string[3][] memory unlockedStamps) internal view returns (string memory) {
+    function buildSvg(string[5][] memory unlockedStamps) internal view returns (string memory) {
         string memory baseUrl = "data:image/svg+xml;base64,";
 
         bytes memory encodedStamps;
         for (uint256 i = 0; i < unlockedStamps.length; i++) {
-            string[3] memory stamp = unlockedStamps[i];
+            string[5] memory stamp = unlockedStamps[i];
 
             bytes memory stampSvg = abi.encodePacked(
-                "<g transform=\"translate(",
+                '<g transform="translate(',
                 abi.encodePacked(stamp[0], ",", stamp[1]),
-                ") scale(1.8)\">",
-                "<path xmlns=\"http://www.w3.org/2000/svg\" fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M20 5v14.054c0 2.303-2.488 3.747-4.488 2.604l-3.016-1.723a1 1 0 0 0-.992 0l-3.016 1.723c-2 1.143-4.488-.3-4.488-2.604V5a3 3 0 0 1 3-3h10a3 3 0 0 1 3 3Z\" fill=\"#323232\"/>",
-                "<text x=\"11.7\" y=\"3.8\" style=\"writing-mode:tb;\" font-weight=\"bold\" stroke=\"black\" font-size=\"7.2\" stroke-width=\"0.1\" fill=\"white\">",
+                ') scale(11)">',
+                '<rect x="0.4375" y="0.4375" width="3.125" height="6.125" ry="1.70625" rx="2" fill="#ffffff" stroke="#',
                 stamp[2],
+                '" stroke-width="0.875" />',
+                '<rect x="0.05" y="0.05" width="3.9" height="6.9" rx="2" ry="2" fill="none" stroke-width="0.15" stroke="#',
+                stamp[2],
+                '"/>',
+                '<text x="1.75" y="3.5" font-family="Meiryo, sans-serif" alignment-baseline="middle" text-anchor="middle" fill="#',
+                stamp[3],
+                '" style="font-size: 8.25%; font-weight: 600; letter-spacing: 0.1; writing-mode: vertical-rl;">',
+                stamp[4],
                 "</text></g>"
             );
 
@@ -205,7 +210,7 @@ contract TokyoExplorer is ERC721A, Ownable {
         );
     }
 
-    function retrieveUnlocks(uint256 tokenId) public view returns (string[3][] memory) {
+    function retrieveUnlocks(uint256 tokenId) public view returns (string[5][] memory) {
         uint256 unlocked = unlocks[tokenId];
 
         // determine size to be declared
@@ -216,7 +221,7 @@ contract TokyoExplorer is ERC721A, Ownable {
             }
         }
 
-        string[3][] memory output = new string[3][](length);
+        string[5][] memory output = new string[5][](length);
 
         uint256 index = 0;
         for (uint256 i = 0; i < stamps.length; i++) {
